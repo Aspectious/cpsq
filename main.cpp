c9dbdea825fa8b1427982be0b3f65587f757974b
aspectious <gdsiebert@outlook.com>
2025-10-12 14:42:59 -0500

    Pluh

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include "include/glfw3.h"
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "2D Renderer", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
+
